fc764338525833ba37576facf9de34e20505f9b3
Moti Main Computer <motigoldstin@gmail.com>
2021-04-06 23:22:46 +0300

    Add tips to tips file.

---
+++ tips-tricks/tips.py
@@ -0,0 +1,20 @@
+# 1) Short if else:
+# Regular if:
+condition = True
+
+if condition:
+    x = 1
+else:
+    x = 0 
+
+# Short if:
+x = 1 if condition else 0
+
+# 2) Write big numbers:
+x = 1000000000
+y = 100000000
+# These numbers equals to the next way of writing, but now you can see the difference:
+x = 1_000_000_000
+y = 100_000_000
+
+        
